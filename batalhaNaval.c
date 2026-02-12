9c66e5a17082a1cfabe9cce1e4c532f9f9dddce1
Lucas Henrique <lucashsilvaa8@gmail.com>
2026-02-11 23:52:11 -0300

    Cone, Cruz e Octaedro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,48 +1,58 @@
 #include <stdio.h>
 
-int main() {
-
-    int tabuleiro[10][10] = {0}; 
-
-    /*
-    NAVIO 1 - VERTICAL
-    */
-    for (int i = 0; i < 4; i++) {
-        tabuleiro[1 + i][2] = 3;
-    }
-
-    /*
-    NAVIO 2 - HORIZONTAL
-    */
-    for (int i = 0; i < 4; i++) {
-        tabuleiro[6][3 + i] = 3;
-    }
-
-    /*
-    NAVIO 3 - DIAGONAL PRINCIPAL
-    */
-    for (int i = 0; i < 4; i++) {
-        tabuleiro[2 + i][2 + i] = 3;
-    }
-
-    /*
-    NAVIO 4 - DIAGONAL SECUNDÁRIA
-    */
-    for (int i = 0; i < 4; i++) {
-        tabuleiro[7 - i][1 + i] = 3;
-    }
-
-    /*
-    Exibição completa do tabuleiro
-    */
-    printf("Tabuleiro 10x10:\n\n");
-
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            printf("%d ", tabuleiro[i][j]);
+void imprimirMatriz(int matriz[5][5]) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            printf("%d ", matriz[i][j]);
         }
         printf("\n");
     }
+}
+
+int main() {
+
+    /*
+    HABILIDADE 1 - CONE
+    */
+    int cone[5][5] = {0};
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 2 - i; j <= 2 + i; j++) {
+            cone[i][j] = 1;
+        }
+    }
+
+    printf("Habilidade Cone:\n");
+    imprimirMatriz(cone);
+    printf("\n");
+
+    /*
+    HABILIDADE 2 - CRUZ
+    */
+    int cruz[5][5] = {0};
+
+    for (int i = 0; i < 5; i++) {
+        cruz[2][i] = 1; 
+        cruz[i][2] = 1; 
+    }
+
+    printf("Habilidade Cruz:\n");
+    imprimirMatriz(cruz);
+    printf("\n");
+
+    /*
+    HABILIDADE 3 - OCTAEDRO
+    */
+    int octaedro[5][5] = {0};
+
+    octaedro[0][2] = 1;
+    octaedro[1][1] = 1;
+    octaedro[1][2] = 1;
+    octaedro[1][3] = 1;
+    octaedro[2][2] = 1;
+
+    printf("Habilidade Octaedro:\n");
+    imprimirMatriz(octaedro);
 
     return 0;
 }
